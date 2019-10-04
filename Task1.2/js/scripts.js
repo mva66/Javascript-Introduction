62f69a32d2013d7f6f8c92c60c06a0aa2a6f0f4e
mva66 <47929128+mva66@users.noreply.github.com>
2019-10-03 21:26:52 -0400

    Create scripts.js

---
+++ Task1.2/js/scripts.js
@@ -0,0 +1,6 @@
+var repository = [
+  { name: "Ivysaur", height: 1, weight: 13, type: ["Grass", "Poison"] },
+  { name: "Nidoking", height: 1.4, weight: 62, type: ["Ground", "Poison"] },
+  { name: "Butterfree", height: 1.1, weight: 32, type: ["Bug", "Flying"] },
+  { name: "Smoochum", height: 0.4, weight: 6, type: ["Psychic", "Ice"] }
+];
