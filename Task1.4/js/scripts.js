e1db8107ca213a8b7dd23e984aab75f1b813f1f7
mva66 <mva6644@gmail.com>
2019-10-06 15:59:37 -0400

    Create scripts.js
    Added ForEach function for Pokemon array

---
+++ Task1.4/js/scripts.js
@@ -0,0 +1,46 @@
+//array created
+var repository = [
+  { name: "Ivysaur", height: 1, weight: 13, type: ["Grass", "Poison"] },
+  { name: "Nidoking", height: 1.4, weight: 62, type: ["Ground", "Poison"] },
+  { name: "Butterfree", height: 1.1, weight: 32, type: ["Bug", "Flying"] },
+  { name: "Smoochum", height: 0.4, weight: 6, type: ["Psychic", "Ice"] }
+];
+
+repository.forEach(function(pokemonlist) {
+  if (pokemonlist.height > 1.1) {
+    document.write(
+      '<div class="pokemon">' +
+        "Name: " +
+        pokemonlist.name +
+        "<br>" +
+        "Weight: " +
+        pokemonlist.weight +
+        "<br>" +
+        " Heigth: " +
+        pokemonlist.height +
+        " - Wow, that's big!" +
+        "<br>" +
+        "Type: " +
+        pokemonlist.type +
+        "<br/>" +
+        "<br/>"
+    );
+  } else {
+    document.write(
+      '<div class="pokemon">' +
+        "Name: " +
+        pokemonlist.name +
+        "<br>" +
+        "Weight: " +
+        pokemonlist.weight +
+        "<br>" +
+        " Heigth: " +
+        pokemonlist.height +
+        "<br>" +
+        "Type: " +
+        pokemonlist.type +
+        "<br/>" +
+        "<br/>"
+    );
+  }
+});
